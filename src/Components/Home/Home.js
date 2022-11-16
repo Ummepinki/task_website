d4cb33ad30aff744c93d559046c92e42667b19a9
Ummepinki <salmaumme888@gmail.com>
2022-11-16 10:21:54 +0600

    new added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,28 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
-import img1 from "../../assets/img-1.png";
+import Banner from "./Banner";
+
 const Home = () => {
   return (
-    <div className="bg-warning m-0 p-0   ">
-      <div className="mx-auto d-flex mt-5 w-50  ">
-        <div className="mt-5 ms-1">
-          <h1>First I wanted to be a veterinarian</h1>
-          <p>
-            I have always loved animals. My desire to help them has led to a
-            possible career path: becoming a veterinarian.
-          </p>
-
-          <Button className="m-2" variant="primary">
-            Contract Us
-          </Button>
-          <Button variant="outline-secondary">Our Service</Button>
-        </div>
-
-        <div>
-          <img className="w-100 h-100  " src={img1} alt="/" />
-        </div>
-      </div>
+    <div>
+      <Banner></Banner>
     </div>
   );
 };
